366f76c32f2371c15a1f1d0de9fb68a78375c5eb
Haskebutterfly <114040707+Haskebutterfly@users.noreply.github.com>
2025-05-16 13:55:57 +1000

    Add files via upload

--- website_backend/scripts/systemtest.js
+++ website_backend/scripts/systemtest.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 async function SystemTest() {
 	try {
 		// Register as an organiser
-		const registerorganiser = await axios.post('http://localhost:5000/api/member/register', {
+		const registerorganiser = await axios.post(`http://localhost:5000/api/member/register`, {
 			name: 'Organisertest',
 			email: 'eventmanagermanagement@gmail.com',
 			password: 'Organisertest2025',
@@ -16,31 +16,91 @@ async function SystemTest() {
 		await new Promise(res => setTimeout(res, 1000));
 
 		// login as an organiser
-		const loginorganiser = await axios.post('http://localhost:5000/api/login', {
+		const loginorganiser = await axios.post(`http://localhost:5000/api/login`, {
 			email: 'eventmanagermanagement@gmail.com',
 			password: 'Organisertest2025',
 			role: 'Organiser'
 		});
 
 		// save organiser logged in details
-		const data = loginorganiser.data;
+		let data = loginorganiser.data;
 		const token = data.token;
 		const organiserid = data.member.memberid;
 		const role = data.member.role;
-
-		console.log("token");
-		console.log(token);
-		console.log("id");
-		console.log(organiserid);
-		console.log("role");
-		console.log(role);
 
 		console.log('Logged in as an Organiser!');
 		// wait a bit, to make sure database is ready
 		await new Promise(res => setTimeout(res, 1000));
 
+		// create an event
+		const createevent = await axios.post(`http://localhost:5000/api/events/organiser`,
+			{
+				name: "Great war re-enactment",
+				date: "2026-05-22",
+				time: "12:36",
+				location: "Kensas City",
+				type: "Camp",
+				description: "wear your favourite WW2 clothes, and grab your props. Head down to Kensas city to re-enact the war. Bring a tent it is a 3 day camp!",
+				generalprice: 30,
+				vipprice: 300,
+				organiser: organiserid
+			},
+			{
+				headers: {
+					Authorization: `Bearer ${token}`
+				}
+			}
+		);
+
+		console.log('Created an event!');
+		// wait a bit, to make sure database is ready
+		await new Promise(res => setTimeout(res, 1000));
+
+		// get all organiser's event
+		const getallorganiserevents = await axios.get(`http://localhost:5000/api/events/organiser`,
+			{
+				params: {
+					organiserid: organiserid
+				},
+				headers: {
+					Authorization: `Bearer ${token}`
+				}
+			}
+		);
+
+		// save the event id
+		data = getallorganiserevents.data;
+		const eventid = data[0]._id;
+
+		console.log('Got all the organise"r events!');
+		// wait a bit, to make sure database is ready
+		await new Promise(res => setTimeout(res, 1000));
+
+		// edit an event
+		const editevent = await axios.put(`http://localhost:5000/api/events/organiser/${eventid}`,
+			{
+				name: "Great war re-enactment",
+				date: "2027-05-22",
+				time: "14:36",
+				location: "Germany",
+				type: "Camp",
+				description: "wear your favourite WW2 clothes, and grab your props. Head down to Kensas city to re-enact the war. Bring a tent it is a 3 day camp!",
+				generalprice: 30,
+				vipprice: 900
+			},
+			{
+				headers: {
+					Authorization: `Bearer ${token}`,
+				},
+			}
+		);
+
+		console.log('Edited an event!');
+		// wait a bit, to make sure database is ready
+		await new Promise(res => setTimeout(res, 1000));
+
 		// Register a user
-		const registeruser = await axios.post('http://localhost:5000/api/member/register', {
+		const registeruser = await axios.post(`http://localhost:5000/api/member/register`, {
 			name: 'Usertest',
 			email: 'hasaanfarache@gmail.com',
 			password: 'Usertest2025',
